4a8eff596c5725380e78f9ffdf9d5bed8c82315e
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2022-08-11 19:26:51 +0900

    Delete : String Cal

--- java-string-calculator/src/main/java/io/github/EcoFriendlyAppleSu/stringCalculator/Application.java
+++ java-string-calculator/src/main/java/io/github/EcoFriendlyAppleSu/stringCalculator/Application.java
@@ -1,13 +1,15 @@
 package io.github.EcoFriendlyAppleSu.stringCalculator;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
-        String str = "1,2:3";
-        String[] split = str.split(",|:");
-        List<String> strings = Arrays.asList(split); // return 1
-        System.out.println(strings);
+
     }
 }
