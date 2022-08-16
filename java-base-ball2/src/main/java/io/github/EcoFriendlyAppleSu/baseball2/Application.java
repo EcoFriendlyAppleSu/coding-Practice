44923270f7c74737ea1f5fe95f9bbb4996a6b719
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2022-08-16 18:44:57 +0900

    Reset : Baseball Game

--- java-base-ball2/src/main/java/io/github/EcoFriendlyAppleSu/baseball2/Application.java
+++
@@ -1,9 +0,0 @@
-package io.github.EcoFriendlyAppleSu.baseball2;
-
-import io.github.EcoFriendlyAppleSu.baseball2.util.ConsoleOutput;
-
-public class Application {
-    public static void main(String[] args) {
-        PlayBaseBall.start();
-    }
-}
