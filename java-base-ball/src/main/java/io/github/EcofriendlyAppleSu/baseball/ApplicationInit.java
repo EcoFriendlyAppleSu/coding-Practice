bf2dc84b90b2a9bb14c5113dc6025f4fec757e34
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2022-12-12 21:54:09 +0900

    baseball game reset

---
+++ java-base-ball/src/main/java/io/github/EcofriendlyAppleSu/baseball/ApplicationInit.java
@@ -0,0 +1,9 @@
+package io.github.EcofriendlyAppleSu.baseball;
+
+public class ApplicationInit {
+
+  public static void main(String[] args) {
+    System.out.println("hello world");
+  }
+
+}
--- java-base-ball/src/main/java/io/github/EcofriendlyAppleSu/baseball/util/GenerateNumber.java
+++
@@ -1,9 +0,0 @@
-package io.github.EcofriendlyAppleSu.baseball.util;
-
-import java.util.List;
-
-@FunctionalInterface
-public interface GenerateNumber {
-
-    List<Integer> generator();
-}
