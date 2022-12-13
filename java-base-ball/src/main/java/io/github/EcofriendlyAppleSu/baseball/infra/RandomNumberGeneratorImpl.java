2c5e1273bdb4f6968292b330e7adca881989cf5d
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2022-12-13 19:07:58 +0900

    feat : random number Generator and test code

--- java-base-ball/src/main/java/io/github/EcofriendlyAppleSu/baseball/infra/RandomNumberGeneratorImpl.java
+++ java-base-ball/src/main/java/io/github/EcofriendlyAppleSu/baseball/infra/RandomNumberGeneratorImpl.java
@@ -1,11 +1,22 @@
 package io.github.EcofriendlyAppleSu.baseball.infra;
 
 import io.github.EcofriendlyAppleSu.baseball.domain.RandomNumberGenerator;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class RandomNumberGeneratorImpl implements RandomNumberGenerator {
 
   @Override
   public String generator() {
-    return null;
+    Set<Integer> randomNumbers = new HashSet<>();
+    Random random = new Random();
+
+    for (; randomNumbers.size() < 3; ) {
+      randomNumbers.add(random.nextInt(9) + 1);
+    }
+    return randomNumbers.stream().map(String::valueOf).collect(Collectors.joining());
   }
 }
